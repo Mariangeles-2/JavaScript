96c23cac8194c3bd9fb2dc45c1c383e64e90eae3
Mariángeles Somma <angie.somma@MacBook-Pro-de-Tomas.local>
2024-10-23 23:29:48 -0300

    agregando condicionales y operadores

---
+++ TOMO 2/condicionsles-y-operadores-js.js
@@ -0,0 +1,17 @@
+//Condicionales if, else if, else
+let calificacion = 75;
+
+if (calificacion >= 90) {
+    console.log("Excelente");
+} else if (calificacion >= 70) {
+    console.log("Bueno");
+} else {
+    console.log("Necesitas estudiar más");
+}
+
+//Operadores de comparación
+//==
+console.log(2 == '2'); // true
+
+//===
+console.log(2 === '2'); // false
