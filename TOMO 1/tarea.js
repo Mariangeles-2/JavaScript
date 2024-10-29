492410330f284d5b85795fbeeca19c9d6670666e
Somma <mariangelessomma@gmail.com>
2024-10-28 21:28:43 -0300

    termino de hacer la tarea, condi8cionales y operadores

--- TOMO 1/tarea.js
+++ TOMO 1/tarea.js
@@ -4,12 +4,21 @@
 
 // Si agregamos el lapso de tiempo, quiero que al final del mensaje diga "Dentro de 4 años vas a tener 21".
 
-const nombre= prompt("Poné tu nombre");
-const apellido = prompt("Poné tu apellido");
-const edad = prompt("Poné tu edad");
-const años = prompt("Poné un lapso de tiempo");
-
-
-const mensaje = "Hola, " + nombre + " " + apellido + ". Bienvenido a nuestra base de datos. Tu edad es " +  edad + " años. Dentro de " + "" + " años vas a tener " + "" + ".";
+let nombre= prompt("Poné tu nombre");
+let apellido = prompt("Poné tu apellido");
+let edad = parseInt(prompt("Poné tu edad"));
+const lapsoDeTiempo = parseInt(prompt("Poné un lapso de tiempo en años"));
+const edadMasLapsoDeTiempo = edad + lapsoDeTiempo;
+let mensaje = "Hola, " + nombre + " " + apellido + ". Bienvenido a nuestra base de datos. Tu edad es " +  edad + " años. Dentro de " + lapsoDeTiempo + " años vas a tener " + edadMasLapsoDeTiempo + ".";
 alert(mensaje);
+console.log(mensaje);
+
+//Poner los datos de pareja
+//Tu novio es Tomas Pais. Su edad es 29 años.
+nombre = prompt("Poné el nombre de tu pareja");
+apellido = prompt("Poné la edad de tu pareja");
+edad = parseInt(prompt("Poné la edad de tu pareja"));
+mensaje = "Tu novio es " + nombre + " " + apellido + ". Su edad es " + edad + " años.";
+alert(mensaje);
+console.log(mensaje);
 
--- TOMO 2/condicionsles-y-operadores-js.js
+++ TOMO 2/condicionsles-y-operadores-js.js
@@ -22,4 +22,45 @@ console.log(2 != '3'); // true
 //!==
 console.log(2 !== '2'); // true
 
+//< <= > >=
 console.log(3 < 4); // true console.log(5 >= 5); // true
+
+//&&
+const esSexoFemenino = true;
+const esMayorDeEdad = true;
+
+if (esSexoFemenino && esMayorDeEdad) {
+    console.log("Es toda una mujer adulta");
+}
+console.log(true && false); // false
+
+//||
+const estoyCansado = true;
+const tengoHambre = false;
+
+if (estoyCansado || tengoHambre) {
+    console.log("No hay SnuSnu");
+}
+console.log(true || false); // true
+
+//!
+const estaLloviendo = false;
+
+if (!estaLloviendo) {
+    console.log("No lleves paraguas");
+} else {
+    console.log("Llevá paraguas");
+}
+console.log(!true); // false
+
+//Ejemplo 1
+
+let edad = 20;
+let acceso = (edad >= 18) && (edad <= 30);
+console.log(acceso); // true si la edad está entre 18 y 30
+
+//Ejemplo 2
+let temperatura = 35;
+let advertencia = (temperatura < 0) || (temperatura > 32);
+console.log(advertencia); // true si la temperatura es menor que 0 o mayor que 32
+
